bf2ad5d8a779c5f9a0dc6ad3e1b3f26446548ced
sony_rich <hoangson200015@gmail.com>
2023-01-16 20:10:01 +0700

    setup the restart automation

--- DayzServer.bat
+++ DayzServer.bat
@@ -9,7 +9,7 @@ set serverPort=2302
 ::Server config
 set serverConfig=serverDZ.cfg
 ::Logical CPU cores to use (Equal or less than available)
-set serverCPU=3
+set serverCPU=4
 ::Sets title for terminal (DONT edit)
 title %serverName% batch
 ::DayZServer location (DONT edit)
@@ -17,8 +17,8 @@ cd "%serverLocation%"
 echo (%time%) %serverName% started.
 ::Launch parameters (edit end: -config=|-port=|-profiles=|-doLogs|-adminLog|-netLog|-freezeCheck|-filePatching|-BEpath=|-cpuCount=)
 start "DayZ Server" /min "DayZServer_x64.exe" -config=%serverConfig% -port=%serverPort% "-profiles=config" "-mod=@CF;@Dabs Framework;@Trader;@DayZ-Expansion-Core;@DayZ-Expansion-Animations;@DayZ-Expansion-Licensed;@DayZ-Expansion-Book;@DayZ-Expansion-Vehicles;@DayZ-Expansion-Navigation;@DayZ-Expansion-Groups;@VPPAdminTools;@ZomBerry Admin Tools;@WSP-TraderLayout;@PristineRepair;@BaseBuildingPlus;@BBPItemPack;@Craft-Gun-Cleaning-Kit;@SNAFU Weapons;@Tactical-Flava;@Repack Ammo;@Furniture-Kits;@MuchStuffPack;@Better Medical Pouch;@RUSForma_vehicles;@Arma 2 Helicopters Remastered;@FlipTransport;@Gas-Pump-Refueling;@InventoryInCar;" -cpuCount=%serverCPU% -dologs -adminlog -netlog -freezecheck
-::Time in seconds before kill server process (14400 = 4 hours)
-timeout 14400
+::Time in seconds before kill server process (14400 = 4 hours, 10800 = 3hrs)
+timeout 10800
 taskkill /im DayZServer_x64.exe /F
 ::Time in seconds to wait before..
 timeout 10
